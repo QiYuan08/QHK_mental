261d0ddaa1bdd607aa37e27aad971c99e8c7c4fb
Teh <qteh0001@student.monash.edu>
2021-07-18 02:20:58 +0800

    Able to store new diary into database from frontend
    - cannot have similar title

--- api.py
+++ api.py
@@ -1,10 +1,20 @@
-from flask import Flask, redirect, url_for, render_template, request
+from flask import Flask, redirect, url_for, render_template, request, session
+import requests
+import json
 
 app = Flask(__name__)
-
+BASE_URL = "http://localhost:4000/"
+app.secret_key = '12312fdfjkqnewfuajndf'
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
+
+    # hardcode user login info
+    session['username'] = 'qiyuna'
+    session['id'] = '60f283e7798ceb3b49b97c0c'
+    session['password'] = 'password'
+
+
     return render_template("main_page.html")
 
 
@@ -16,6 +26,32 @@ def instruction():
 @app.route("/diary", methods=['GET', 'POST'])
 def diary():
     return render_template("diary_display.html")
+
+
+@app.route("/upload", methods=['POST'])
+def upload():
+
+    isPublic = False
+    if request.form['submitBtn'] == 'Private':
+        isPublic = False
+    else:
+        isPublic = True
+
+    data = {
+        'title':  request.form['title'],
+        'content': request.form['text'],
+        'owner': session['id'],
+        'isPublic': isPublic,
+        'dateCreated': request.form['date'].replace('-', '')
+    }
+    headers = {u'content-type': u'application/json'}
+
+    response = requests.post(BASE_URL + 'diary/newDiary', data=json.dumps(data), headers=headers)
+
+    if response.status_code == '200':
+        return True
+    else: 
+        return response.json()
 
 
 @app.route("/diary_entry", methods=['GET', 'POST'])
